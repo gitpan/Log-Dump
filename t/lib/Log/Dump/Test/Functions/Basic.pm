084c59959e416e96bd237f38e764187457dd1e1a
Kenichi Ishigaki <ishigaki@cpan.org>
2011-04-07 05:38:17 +0000

    Import of ISHIGAKI/Log-Dump-0.08 from CPAN.
    gitpan-cpan-distribution: Log-Dump
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         ISHIGAKI/Log-Dump-0.08.tar.gz
    gitpan-cpan-author:       ISHIGAKI
    gitpan-cpan-maturity:     released

--- t/lib/Log/Dump/Test/Functions/Basic.pm
+++ t/lib/Log/Dump/Test/Functions/Basic.pm
@@ -15,7 +15,7 @@ sub initialize {
   $class->capture( IO::Capture::Stderr->new );
 }
 
-sub plain_usage : Test {
+sub plain_usage : Tests(2) {
   my $class = shift;
 
   my $capture = $class->capture;
@@ -23,9 +23,39 @@ sub plain_usage : Test {
   $capture->start;
   log( debug => 'message' );
   $capture->stop;
+  my $captured = join "\n", $capture->read;
 
-  like $capture->read => qr/\[debug\] message/,
+  like $captured => qr/\[debug\] message/,
     $class->message('captured');
+  unlike $captured => qr{Log.Dump.Functions},
+    $class->message('not from Log::Dump::Functions');
+}
+
+sub error : Tests(2) {
+  my $class = shift;
+
+  my $capture = $class->capture;
+
+  $capture->start;
+  log( error => 'message' );
+  $capture->stop;
+  my $captured = join "\n", $capture->read;
+
+  like $captured => qr/\[error\] message/,
+    $class->message('captured');
+  unlike $captured => qr{Log.Dump.Functions},
+    $class->message('not from Log::Dump::Functions');
+}
+
+sub fatal : Tests(2) {
+  my $class = shift;
+
+  eval { log( fatal => 'message' ) };
+
+  like $@ => qr/\[fatal\] message/,
+    $class->message('captured');
+  unlike $@ => qr{Log.Dump.Functions},
+    $class->message('not from Log::Dump::Functions');
 }
 
 1;
